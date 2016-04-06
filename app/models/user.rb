919d46e371ef2f2377b3ba48001737ecf9384ee9
Alexey Levashev <alex.levashev@cashplay.com>
2016-04-06 13:05:23 +0200

    Opps!

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :tasks, dependent: :destroy
-  
+
   def alltasks
     user = self.id
     Task.where(:assignee => user)
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160325205209) do
+ActiveRecord::Schema.define(version: 20160406110037) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "tasks", force: :cascade do |t|
     t.integer  "user_id"
@@ -25,7 +28,7 @@ ActiveRecord::Schema.define(version: 20160325205209) do
     t.datetime "endtime"
   end
 
-  add_index "tasks", ["user_id"], name: "index_tasks_on_user_id"
+  add_index "tasks", ["user_id"], name: "index_tasks_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
@@ -45,9 +48,10 @@ ActiveRecord::Schema.define(version: 20160325205209) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.boolean  "active"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
